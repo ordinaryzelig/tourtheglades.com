56f4e28ee4e6d479393b186888bca08946dafb9b
Jared Ning <jared@redningja.com>
2014-05-27 18:03:44 -0400

    Fix bug with facebook gallery item clicking.

--- assets/gallery_facebook_item.js
+++ assets/gallery_facebook_item.js
@@ -2,5 +2,9 @@ jQuery(function() {
   // Removed gallery-item class for facebook icon so that
   // zoomOverlay doesn't do anything to it.
   // But add the class back after page load so it gets to keep styling.
-  jQuery(".gallery-item-facebook").addClass('gallery-item');
+  // Also disable the cbox jQuery plugin so clicking will actually send user to facebook.
+  jQuery(".gallery-item-facebook")
+    .addClass('gallery-item')
+    .find('a.cboxElement')
+    .removeClass('cboxElement');
 });
